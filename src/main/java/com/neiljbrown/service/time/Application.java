27eef0763707f3396befef9d2b52c6a9311db6a2
Neil Brown <neil.brown.git@gmail.com>
2019-02-28 21:27:24 +0000

    Convert container-based backend Java time-service to AWS serverless app using AWS serverless Java container (ctd) - Modify @SpringBootApplicaton Class to Extend Spring WebApplicationInitializer as a means of supporting deploying and running a Boot app when packaged in a Lambda specific zip file.

--- src/main/java/com/neiljbrown/service/time/Application.java
+++ src/main/java/com/neiljbrown/service/time/Application.java
@@ -17,6 +17,8 @@ package com.neiljbrown.service.time;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Provides the entry point for running the application from the command line.
@@ -29,9 +31,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * <br>
  * - {@code @EnableAutoConfiguration} - Enables Spring Boot's intelligent convention-over-configuration behaviour for
  * guessing and configuring the Spring beans the app is likely to need, based on its environment, classpath etc.
+ *
+ * <h2>Running a Spring Boot App on AWS Lambda Using the AWS Serverless Java Container Library</h2>
+ * The AWS Serverless Java Container library requires an {@link SpringBootApplication} class such as this to
+ * additionally implement Spring's {@link org.springframework.web.WebApplicationInitializer}. Spring Boot apps
+ * typically do this to support deploying the app to a central web container as a WAR file, rather than running it
+ * from the command line from a standalone JAR that includes an embedded web container, by extending
+ * {@link SpringBootServletInitializer} and overriding its
+ * {@link SpringBootServletInitializer#configure(SpringApplicationBuilder)} method. The AWS Serverless Java Container
+ * Library uses this Spring mechanism to instead support deploying your app to Lambda, using the service's custom format
+ * zip file (rather than a WAR). And whilst the AWS library runs an embedded Jetty server, it does not use Spring
+ * Boot to support this. Also, notably, (according to the example AWS library's Spring Boot starter app -
+ * https://github.com/awslabs/aws-serverless-java-container/wiki/Quick-start---Spring-Boot) it is however not
+ * necessary to override {@link SpringBootServletInitializer#configure(SpringApplicationBuilder)}.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
   // Prevent instantiation of class while it is a utility class containing only static methods, to keep checkstyle happy
   protected Application() {
